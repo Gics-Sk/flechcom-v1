79f0aaf69157fe67e3f21ed5855ac6c5471d95b6
Giovanni <giovanni.sokenou2105@gmail.com>
2024-03-01 17:32:53 +0100

    six

--- script.js
+++ script.js
@@ -15,6 +15,35 @@ function togglePasswordVisibility() {
     }
 }
 
+function togglePasswordVisibility1() {
+    var passwordInput = document.getElementById("password1");
+    var icon = document.querySelector('.password-toggle i');
+
+    if (passwordInput.type === "password1") {
+        passwordInput.type = "text";
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = "password1";
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
+function togglePasswordVisibility2() {
+    var passwordInput = document.getElementById("password2");
+    var icon = document.querySelector('.password-toggle i');
+
+    if (passwordInput.type === "password2") {
+        passwordInput.type = "text";
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = "password2";
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
+
 // SCRIPT POUR LA NAVBAR TOOGLE COULISSANT DE DROITE
 document.querySelector('.navbar-toggler').addEventListener('click', function () {
     document.querySelector('.navbar').classList.toggle('show');
